f90e0bf178dea86076b3f8b1bdce67319c42d22b
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-04-16 12:07:37 -0400

    feat(db): setup db.

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,14 +2,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+interface DBConfig {
+  mongodbUri: string;
+  name: string;
+};
+
 interface Config {
   port: number;
   nodeEnv: string;
-}
-
-const config: Config = {
-  port: Number(process.env.PORT) || 3000,
-  nodeEnv: process.env.NODE_ENV || 'development',
+  db: DBConfig;
 };
 
-export default config;
+export const config: Config = {
+  port: Number(process.env.PORT) || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
+  db: {
+    mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017',
+    name: process.env.DB_NAME || 'ecommerce',
+  }
+};
