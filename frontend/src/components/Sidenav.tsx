815937e49584f8c6b3e9c6c241bb7967a625d4c3
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-04-23 15:44:36 -0400

    feat: frontend setup.

---
+++ frontend/src/components/Sidenav.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { logo } from "../utils/data";
+import { navItems } from "../utils/data";
+
+interface SidenavProps {
+  styles?: string;
+}
+const Sidenav = ({styles}: SidenavProps) => {  
+  
+  return (
+    <nav 
+      className={`    
+        ${styles}
+        flex flex-col gap-8 overflow-hidden shadow-xs 
+        border-r border-[#e5e7eb] w-64 min-h-screen h-full px-6 bg-[var(--bg-nav)]
+      `}
+      >
+      <h1>{logo}</h1>
+      <ul className="flex flex-col gap-6">
+        <li>
+          <Link to={navItems[0].link}>
+            <h3>{navItems[0].name}</h3>
+          </Link>
+        </li>                
+
+      </ul>
+    </nav>
+  );
+};
+
+export default Sidenav;
