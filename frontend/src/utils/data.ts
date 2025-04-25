650acc0b09c9a154f873c09f7c573e8416ccafa1
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-04-25 13:41:46 -0400

    fix(products-upload-ui): fix api url.

--- frontend/src/utils/data.ts
+++ frontend/src/utils/data.ts
@@ -1,3 +1,5 @@
+export const API_URL = 'http://localhost:3000/api';
+
 export { default as avatarImg }  from '../assets/images/avatar.jpg';
 export const logo = "LOGO";
 export const adminName = 'Blue Water';
