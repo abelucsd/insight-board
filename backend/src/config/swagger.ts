381f4ece329865a746b204293f058f05aae3bb35
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-04-30 14:19:54 -0400

    feat(api-docs): add swagger api docs for invoice analytics.

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -9,6 +9,8 @@ export const swaggerDocument = yaml.load(fs.readFileSync(docsPath, 'utf8'));
 const productPaths = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/paths/products.yaml'), 'utf8'))
 const categoryPaths = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/paths/category.yaml'), 'utf8'))
 const invoicePaths = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/paths/invoice.yaml'), 'utf8'))
+const invoiceAnalyticsPaths = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/paths/invoiceAnalytics.yaml'), 'utf8'))
+
 const productComponents = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/components/schemas/product.yaml'), 'utf8'))
 const categoryComponents = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/components/schemas/category.yaml'), 'utf8'))
 const invoiceComponents = yaml.load(fs.readFileSync(path.join(__dirname, '../../docs/components/schemas/invoice.yaml'), 'utf8'))
@@ -17,6 +19,7 @@ swaggerDocument.paths = {
   ...productPaths,
   ...categoryPaths,
   ...invoicePaths,
+  ...invoiceAnalyticsPaths,
 };
 swaggerDocument.components = {
   schemas:{
