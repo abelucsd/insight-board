bd8a6192a3717a2c54273f66712a82bea9bede74
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-06-06 17:59:24 -0400

    feat: deploy redis
    * Feat/products table UI (#64)
    
    - Includes:
      * Table component with server side filtering and pagination.
      * Products view table.
      * Updated products test to match pagination and filtering.
    
    * Feat/add profit and locations analytics (#69)
    
    - Includes:
      * end to end workflow
      * tests
    
    * Feat(visit-analytics) (#70)
    
    - Includes:
      * visit e2e workflow
      * visit stat box
    
    * db(load-invoices): load real invoices via faker python script. (#71)
    
    - Includes:
      * Modify load clean and validation.
    
    * Feat/invoice table (#72)
    
    - Includes:
      * Implement invoice table ui.
      * Refactor Invoice controller functions to work with table requirements.
      * Refactor Invoice tests to work with new controller return structure.
    
    * deploy: modify server and storefront urls for deployment. (#73)
    
    * feat: deploy redis
    
    * fix: redis
    
    * chore: remove default export on redisClient.

--- backend/src/redis/redisClient.ts
+++ backend/src/redis/redisClient.ts
@@ -1,6 +1,9 @@
-import Redis from "ioredis";
-export const redis = new Redis({
-  host: '127.0.0.1',
-  port: 6379,
-  maxRetriesPerRequest: null,
+import Redis from 'ioredis';
+
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL!, {
+  tls: {}  // Enables TLS for rediss://
+});
+
+redis.on('error', (err) => {
+  console.error('❌ Redis error:', err);
 });
