3bf484ca00d9e2de8e07595ab22ac0c9bf29f6ab
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-06-10 11:52:33 -0400

    feat(redis-deployment): fix redisClient for deployment.

--- backend/src/redis/redisClient.ts
+++ backend/src/redis/redisClient.ts
@@ -1,9 +1,14 @@
-import Redis from 'ioredis';
+require('dotenv').config();
+const Redis = require('ioredis');
 
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL!, {
   maxRetriesPerRequest: null,
 });
 
-redis.on('error', (err) => {
+redis.on('connect', () => {
+  console.log('Successfully connected to Upstash Redis!');
+});
+
+redis.on('error', (err: Error) => {
   console.error('❌ Redis error:', err);
 });
