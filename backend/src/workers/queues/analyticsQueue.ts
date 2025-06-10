37529889af3ee4b208f8ef953f74d5eeef01423a
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-06-10 12:59:34 -0400

    fix(redis-deployment): modify bullmq connection to upstash instead of redis. (#87)

--- backend/src/workers/queues/analyticsQueue.ts
+++ backend/src/workers/queues/analyticsQueue.ts
@@ -5,7 +5,12 @@ let analyticsQueue: Queue | null = null;
 
 export const getAnalyticsQueue = (): Queue => {
   if (!analyticsQueue) {
-    analyticsQueue = new Queue('analytics', {connection: redis});
+    analyticsQueue = new Queue('analytics', {
+      connection: {
+        url: process.env.UPSTASH_REDIS_URL,
+        maxRetriesPerRequest: null,
+      }
+    });
   }
   return analyticsQueue;
 };
