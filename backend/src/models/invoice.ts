ee7d183c292a9f11ffaf12698a9333f1596e68a4
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-07-02 16:54:43 -0400

    Refactor/invoice v2 (#98)
    - Inclues:
      * Refactor invoice swagger, model, strategy, tests, and table ui.

--- backend/src/models/invoice.ts
+++ backend/src/models/invoice.ts
@@ -2,14 +2,15 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 export interface CreateInvoiceInput {
+  id: string
   customer: string;
   itemName: string;
-  itemNumber: number;  
+  itemNumber: string;  
   price: number;
   date: string;
   quantity: number;
   revenue: number;
-  totalCost: number;
+  cost: number;
   profit: number;
   location: string;
 };
@@ -19,14 +20,15 @@ export interface IInvoice extends CreateInvoiceInput {
 };
 
 const invoiceSchema = new Schema<IInvoice>({
+  id: { type: String, required: true},
   customer: { type: String, required: true},
   itemName: { type: String, required: true},
-  itemNumber:{ type: Number, required: true},
+  itemNumber:{ type: String, required: true},
   price: { type: Number, required: true},
   date: { type: String, required: true},
   quantity: { type: Number, required: true},
   revenue: { type: Number, required: true},
-  totalCost:{ type: Number, required: true},
+  cost:{ type: Number, required: true},
   profit: { type: Number, required: true},
   location: { type: String, required: true},
 });
--- backend/tests/invoice/invoice.int.test.ts
+++ backend/tests/invoice/invoice.int.test.ts
@@ -13,26 +13,28 @@ describe('Invoice Integration', () => {
 
   const invoices = [
     {      
+      id: 'inv-01',
       customer: "Foo 1", 
       itemName: "Bar 2",
-      itemNumber: 123,
+      itemNumber: 'prod-01',
       price: 5,
       date: new Date().toDateString(),
       quantity: 10,
       revenue: 50,
-      totalCost: 10,
+      cost: 10,
       profit: 40,
       location: 'USA',
     },
     {      
+      id: 'inv-02',
       customer: "Foo 2", 
       itemName: "Bar 2",
-      itemNumber: 123,
+      itemNumber: 'prod-02',
       price: 10,
       date: new Date().toDateString(),
       quantity: 20,
       revenue: 200,
-      totalCost: 40,
+      cost: 40,
       profit: 160,
       location: 'USA',
     }
@@ -90,6 +92,7 @@ describe('Invoice Integration', () => {
       const {data, total} = await invoiceService.getInvoices('', 1, 10);
       // cleanup mongoose added variables
       const cleanedresult = data.map((item) => ({
+        id: item.id,
         customer: item.customer,
         itemName: item.itemName,
         itemNumber: item.itemNumber,
@@ -97,7 +100,7 @@ describe('Invoice Integration', () => {
         date: item.date,
         quantity: item.quantity,
         revenue: item.revenue,
-        totalCost: item.totalCost,
+        cost: item.cost,
         profit: item.profit,
         location : item.location,
       }));
