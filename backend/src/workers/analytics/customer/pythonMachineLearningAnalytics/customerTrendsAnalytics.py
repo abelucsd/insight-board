21162d01d04cff43d0717d46a9ffd0b0cf39e884
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-07-24 06:04:14 -0400

    feat(integrate-backend-python-ml): wrap args in try except block.

--- backend/src/workers/analytics/customer/pythonMachineLearningAnalytics/customerTrendsAnalytics.py
+++ backend/src/workers/analytics/customer/pythonMachineLearningAnalytics/customerTrendsAnalytics.py
@@ -101,11 +101,11 @@ if __name__ == '__main__':
    if len(sys.argv) < 4:
       print(f"ERROR: Insufficient number of arguments passed to the python script.", file=sys.stderr)
       sys.exit(1)
-      
-   mongo_uri = sys.argv[1]
-   db_name = sys.argv[2]
-   analysis_type = sys.argv[3]
    try:
+      mongo_uri = sys.argv[1]
+      db_name = sys.argv[2]
+      analysis_type = sys.argv[3]
+   
       result = run_ml_analysis(mongo_uri, db_name, analysis_type)
       print(json.dumps(result))
    except Exception as e:
